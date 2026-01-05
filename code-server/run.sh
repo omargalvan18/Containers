595ba48fe106327f5362572db94f709c005b1242
Omar Galvan <ogben19@hotmail.com>
2026-01-05 11:44:44 -0600

    Add env file

--- code-server/run.sh
+++ code-server/run.sh
@@ -1,8 +1,8 @@
 docker run -it --rm \
   -p 8080:8080 \
-  -e PASSWORD=${PASSWORD} \
   -v "$(pwd)":"/home/coder/project" \
   -u "$(id -u):$(id -g)" \
   -e "DOCKER_USER=$USER" \
+  --env-file .env \
   --hostname vscode \
   java_docker
