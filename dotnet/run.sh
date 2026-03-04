5a659666180e661919dcc92d24410867f1a23949
Omar Galvan <ogben19@hotmail.com>
2026-03-04 00:10:13 -0600

    Implement dotnet container

---
+++ dotnet/run.sh
@@ -0,0 +1,9 @@
+docker run -it --rm \
+  -e UID=$UID \
+  -e GID=$GID \
+  -e USERNAME=$USER \
+  -p 5232:5232 \
+  -v "$(pwd)":"/home/project" \
+  -w /home/project \
+  --hostname dotnet \
+  dotnet
