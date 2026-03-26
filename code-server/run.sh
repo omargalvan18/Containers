43e6db87e6cf0c3d0aa8d286f1c4dd76e672c1d7
Omar Galvan <ogben19@hotmail.com>
2026-03-26 15:36:08 -0600

    Mount settings.json to persist themes

--- code-server/run.sh
+++ code-server/run.sh
@@ -1,6 +1,7 @@
 docker run -it --rm \
   -p 8080:8080 \
-  -v "$(pwd)":"/home/coder/project" \
+  -v $(pwd):/home/coder/project \
+  -v $(pwd)/settings.json:/home/coder/.local/share/code-server/User/settings.json \
   -u "$(id -u):$(id -g)" \
   -e "DOCKER_USER=$USER" \
   --env-file .env \
